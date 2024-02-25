66fa7f81b7d2bd304bdaa8d2aa9e0c6adcefad5a
AlogoZano <160699916+AlogoZano@users.noreply.github.com>
2024-02-25 14:10:22 -0600

    Paquete corregido
    Se cambió el nombre del paquete

---
+++ Challenges/Challenge_1/courseworks/courseworks/signal_generator.py
@@ -0,0 +1,43 @@
+import rclpy
+import math as ma
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
+
+class Signal_Generator(Node):
+
+    def __init__(self):
+        super().__init__('signal_generator_node')
+        self.publisher_signal = self.create_publisher(Float32, 'signal', 10)
+        self.publisher_time = self.create_publisher(Float32, 'time', 10)
+
+        self.timer_period = 0.1
+        self.time = 0
+
+        self.msg_signal = Float32()
+        self.msg_time = Float32()
+
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+        self.get_logger().info('Signal Generator node successfully initialized!')
+
+    def timer_callback(self):
+        self.time += self.timer_period
+
+        self.msg_signal.data = ma.sin(self.time) 
+        self.publisher_signal.publish(self.msg_signal)
+
+        self.msg_time.data = self.time
+        self.publisher_time.publish(self.msg_time)
+        self.get_logger().info('Tiempo: {} Senial: {}'.format(self.time, self.msg_signal.data))
+
+
+def main(args = None):
+    rclpy.init(args=args)
+    m_s = Signal_Generator()
+    rclpy.spin(m_s) #While(1)
+    m_s.destroy_node() #Destruir nodo
+    rclpy.shutdown()    
+
+if __name__ == '__main__':
+    main()
---
+++ Challenges/Challenge_1/courseworks/launch/signal_process_launch.py
@@ -0,0 +1,19 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    signal_node  = Node(package = 'courseworks',  
+                        executable='signal_generator',
+                        output = 'screen')
+    processed_signal_node  = Node(package = 'courseworks',    
+                                  executable='process',   
+                                  output = 'screen')
+    rqt_plot_node  = Node(package = 'rqt_plot',    
+                          executable='rqt_plot',    
+                          output = 'screen')
+    rqt_graph_node  = Node(package = 'rqt_graph',    
+                          executable='rqt_graph',    
+                          output = 'screen')
+    ld = LaunchDescription ([signal_node,processed_signal_node,rqt_plot_node,rqt_graph_node])
+    return ld
